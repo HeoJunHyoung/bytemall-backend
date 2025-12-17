67445c85df62501068e845ed00cf2042dffd6ac8
junbro <gjwnsgud4016@gmail.com>
2025-12-17 16:56:35 +0900

    init project

---
+++ src/main/java/com/example/bytemallbackend/BytemallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bytemallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytemallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytemallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bytemallbackend/BytemallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bytemallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytemallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
