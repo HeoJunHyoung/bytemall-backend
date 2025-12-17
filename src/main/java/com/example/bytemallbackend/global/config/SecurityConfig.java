3a48235a03d2a5081edfbcda6057513e194c4511
junbro <gjwnsgud4016@gmail.com>
2025-12-17 22:15:20 +0900

    feat: Configuration 설정 추가
    - CorsConfig: cookie 기반의 JWT 사용 예정이라 setAllowCredentials(true) 설정
    - EncoderConfig(BCrytPasswordEncoder): 빈 참조 순환 방지를 위해 별도의 Configuration으로 분리
    - RedisConfig: Lattuce 기반의 Redis (문자열 전용 / DTO 캐싱 전용 RedisTemplate 정의)
    - SecurityConfig: httpBasic, csrf, formLogin, logout 들은 disable / session 상태는 Stateless로 관리(JWT 도입 예정)

---
+++ src/main/java/com/example/bytemallbackend/global/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.bytemallbackend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable);
+
+
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**", "/login/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/admin/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+
+    }
+
+}
