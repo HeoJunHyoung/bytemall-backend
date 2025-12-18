cbb1d41c6380039f6865b6c68275175a8386c55c
junbro <gjwnsgud4016@gmail.com>
2025-12-18 15:03:56 +0900

    chore: 내 정보 조회 api 경로 수정

--- src/main/java/com/example/bytemallbackend/domain/member/controller/MemberController.java
+++ src/main/java/com/example/bytemallbackend/domain/member/controller/MemberController.java
@@ -17,7 +17,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping
+    @GetMapping("/me")
     public ResponseEntity<MemberResponse> getMyInfo(@AuthenticationPrincipal AuthMember authMember) {
         MemberResponse memberResponse = memberService.getMyInfo(authMember.getId());
         return ResponseEntity.ok(memberResponse);
